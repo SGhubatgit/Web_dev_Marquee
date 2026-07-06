eebb8ccbc47fa3a96499c6192763fdb31ca296a5
[Sunny Gautam] <sunnygautam16082003@gmail.com>
2026-07-06 10:47:19 +0530

    javaScript

---
+++ JavaScript/typeconversion.js
@@ -0,0 +1,14 @@
+debugger;
+
+
+var globalVariable = "I am a global variable";
+
+console.log(globalVariable);
+console.log(globalFunction);
+
+
+console.log("Global Execution Context ends");
+
+var globalFunction = function () {
+    console.log("Inside global function");
+};
---
+++ JavaScript/variable.js
@@ -0,0 +1,17 @@
+// console.log(name);
+
+// var name = "Tushar";
+
+// greet();
+
+// function greet() {
+//     console.log("Hello, i am sunny gautam");
+// }
+
+
+// {
+//     console.log(name);
+//     let name = 'sunny';
+// }
+
+// console.log(12*147569666656)
